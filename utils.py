a58c50da8c5d826574857e00efc3580d2d4694a6
Bob B <bob1b@users.noreply.github.com>
2023-10-07 20:28:52 -0700

    fix shorten() util method

--- utils.py
+++ utils.py
@@ -4,19 +4,16 @@ from flask import g
 _logger = logging.getLogger(__name__)
 
 
-def shorten(value, max_len=None):
+def shorten(value, max_len=30):
+    placeholder = '...'
     if value is None or value == '':
         return ''
-    length = len(value)
-    if max_len is None:
-        max_len = 30
-    if length <= max_len or max_len < 5:
+    if len(value) <= max_len or max_len < (len(placeholder) + 2):
         return value
-    first_part_len = -(-max_len - 3) // 2  # round up division using two minuses
-    last_part_len = (max_len - 3) - first_part_len
-    first_part = value[:first_part_len]
-    last_part = value[-last_part_len:]
-    return f"{first_part}...{last_part}"
+    max_len_minus_placeholder = max_len - len(placeholder)
+    last_part_len = max_len_minus_placeholder // 2  # round up division
+    first_part_len = max_len_minus_placeholder - last_part_len
+    return f"{value[:first_part_len]}{placeholder}{value[-last_part_len:]}"
 
 
 def clear_g_data():
