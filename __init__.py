decbaa84e5f9e3d8d3e77d6affe2c9479230d890
Bob B <bob1b@users.noreply.github.com>
2023-10-13 15:28:08 -0500

    add some todos

--- __init__.py
+++ __init__.py
@@ -3,12 +3,38 @@ __version__ = ".1"
 from . import jwt_user
 from . import view_decorators
 
+# TODO #####################################
+# TODO - race condition problem
+#   * Two or more requests using the same access token (and refresh token) call an endpoint
+#   * The first request is processed first and gets a refreshed access token [cookie]. The old token is set as
+#     just-expired in the db
+#   * The second request starts before the token is changed in the db, so this request is also going to get a
+#     new access token cookie, and the old token will again be set as just-expired
+#   * Since currently tokens are replaced when they expire, the next request is using an invalid token,
+#     leading to the user getting logged out
+
+# TODO - Test case:
+#   *  Malicious user steals an access token and can act as a logged-in user until the access token needs to be
+#      refreshed.
+#   * When then malicious user's access token expires, it can't be refreshed, and the user is logged out. Optionally,
+#     we can invalidate all of that user's session's tokens OR all of that user's tokens
+
+#   TODO - refresh the access tokens early and leave the access token in the table as a just-expired token
+#     TODO - ensure that the request has a valid refresh token
+#   TODO - remove just-expired access token records after they are older than refresh token expiration (they can't be
+#          refreshed) OR after the just-expired window ends
+#              This is the problematic part. How do we know if there are any pending requests that are using the old
+#              token?
+#   TODO - intermittently consolidate access tokens having the same refresh token (or session uuid)
+#     TODO - use the refresh token as a session uuid
+#     TODO - send the new token and set the consolidate tokens to expire soon
+
 # TODO - don't refresh tokens if the user has been logged out
 # TODO - packaging
 # TODO - reenable CSRF
 # TODO _ figure out why flask.g (and/or the ctx) isn't getting cleared/popped after the request ends
 # TODO - unit tests
-
+# TODO #####################################
 
 """
   Files:
